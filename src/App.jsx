d034f70195825d3b1e5c854d54de5e8a6ee854c9
hannes <hannes.mitterer@gmail.com>
2025-08-23 01:49:21 -0400

    Update App.jsx to import and render MobileDeployButton at the top of the landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MobileDeployButton from './MobileDeployButton';
+
+function App() {
+  return (
+    <div>
+      <MobileDeployButton />
+      {/* Existing content goes here */}
+    </div>
+  );
+}
+
+export default App;
